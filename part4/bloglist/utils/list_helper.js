0e99f5adf70b529ae891c7e1b5fbc83e00764064
Steve Hommy <steve96hommy@gmail.com>
2023-10-25 15:23:26 +0300

    Part 4.5 done

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -6,12 +6,30 @@ const totalLikes = (blogs) => {
     const reducer = (sum, item) => {
         return sum + item.likes
     }
+
     return blogs.length === 0
         ? 0
         : blogs.reduce(reducer, 0)
 }
 
+const favoriteBlog = (blogs) => {
+    if (blogs.length === 0) {
+        return null
+    }
+
+    const favorite = blogs.reduce((prev, curr) => {
+        return prev.likes > curr.likes ? prev : curr
+    })
+
+    return {
+        title: favorite.title,
+        author: favorite.author,
+        likes: favorite.likes
+    }
+}
+
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }
