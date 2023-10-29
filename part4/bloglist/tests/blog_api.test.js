74b27406fe88f83a4584e088d62a7ed35164e773
Steve Hommy <steve96hommy@gmail.com>
2023-10-29 14:06:38 +0200

    Part 4.9 done

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -4,13 +4,39 @@ const app = require('../app')
 
 const api = supertest(app)
 
-describe('HTTP GET blog list', () => {
+const Blog = require('../models/blog')
+const helper = require('./test_helper')
 
-    test('blogs are returned as json', async () => {
+beforeEach(async () => {
+    await Blog.deleteMany({})
+    await Blog.insertMany(helper.initialBlogs)
+})
+
+describe('Blog API GET Requests', () => {
+    test('Return blogs as JSON', async () => {
         await api
             .get('/api/blogs')
             .expect(200)
             .expect('Content-Type', /application\/json/)
+    })
+
+    test('All blogs are returned', async () => {
+        const response = await api.get('/api/blogs')
+        expect(response.body).toHaveLength(helper.initialBlogs.length)
+    })
+
+    test('A specific title is within the returned blogs', async () => {
+        const response = await api.get('/api/blogs')
+        const blogs = response.body.map(blog => blog.title)
+        expect(blogs).toContain('Full Stack Open Part4')
+    })
+
+    test('Verify the existence of the id property', async () => {
+        const response = await api.get('/api/blogs')
+        const blogs = response.body
+        blogs.forEach(blog => {
+            expect(blog.id).toBeDefined()
+        })
     })
 })
 
