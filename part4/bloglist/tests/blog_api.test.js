01824b7813b0e3bc72ec414ca3a054a9e34e9fc6
Steve Hommy <steve96hommy@gmail.com>
2023-10-30 14:19:25 +0200

    Part 4.13 done

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -114,7 +114,23 @@ describe('Blog API POST Requests', () => {
             .send(newBlog)
             .expect(400)
     })
+})
 
+describe('Blog API DELETER Request', () => {
+    test('Deleting a single blog with status code 204 if id is valid', async () => {
+        const blogAtStart = await helper.blogsInDB()
+        const blogToDelete = blogAtStart[0]
+
+        await api
+            .delete(`/api/blogs/${blogToDelete.id}`)
+            .expect(204)
+
+        const blogsAtEnd = await helper.blogsInDB()
+        expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length - 1)
+
+        const titles = blogsAtEnd.map(blog => blog.title)
+        expect(titles).not.toContain(blogToDelete.title)
+    })
 })
 
 afterAll(async () => {
