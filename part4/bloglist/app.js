3cf35050a690701c51e0ec6d21c15217d088a08e
Steve Hommy <steve96hommy@gmail.com>
2023-11-04 22:30:09 +0200

    Part 4.18 done

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -7,6 +7,7 @@ const mongoose = require('mongoose')
 
 const blogsRouter = require('./controllers/blogs')
 const userRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 const middleware = require('./utils/middleware')
 
 mongoose.set('strictQuery', false)
@@ -28,6 +29,7 @@ app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', userRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.unkownEndpoint)
 app.use(middleware.errorHandler)
